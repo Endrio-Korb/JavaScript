4d62dc6496f2b1823f4f8227721c4ae1c5297ee9
Endrio-Korb <alexkorb95@gmail.com>
2023-08-12 18:15:34 -0300

    .

--- Aula2/js/copia.js
+++ Aula2/js/copia.js
@@ -1,39 +1,68 @@
-const { element } = require("prop-types");
+// const { element } = require("prop-types");
 
-const numbers = [1,2,3,4,5];
-const evenNumbers = numbers.filter(num => num % 2 === 0);
+const { func } = require("prop-types");
 
-function fatorial(n) {
-    if (n <= 1){
-        return 1;
+// const numbers = [1,2,3,4,5];
+// const evenNumbers = numbers.filter(num => num % 2 === 0);
+
+// function fatorial(n) {
+//     if (n <= 1){
+//         return 1;
+//     }
+//     else {
+//         return n * factorial(n - 1);
+//     }
+// }
+
+// function add(a,b) {
+//     return a + b;
+// }
+
+// const numbers1 = [1,2,3];
+// const dobledNumbers = numbers1.map(num => num * 2);
+
+
+// const add = x => x + 1;
+// const multiply = x => x * 2;
+// const composedFunction = compose(add, multiply);
+// console.log(composedFunction(3)); // Primeiro multiplica por 2 e depois adiciona 1
+
+// const add1 = x => x + y;
+// const add5 = add(5);
+// console.log(add5(3)); // Resultado: 8
+
+// const numbers2 = [1,2,3,4,5];
+// const sum = numbers2.reduce((acc,num) => acc + num, 0)
+
+// const newArray = oroginalArray.map(/* element =>  tranformação */);
+
+// const numbers3 = [1,2,3,4,5];
+// const squaredNumbers = numbers3.map(num => num * num);
+// console.log(squaredNumbers);
+
+async function fetchData() {
+    const cpf = document.getElementById("cpf")
+    const url = `https://viacep.com.br/ws/${cpf}/json/`;
+
+    //const url = 'https://viacep.com.br/ws/01001000/json/';
+
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.status}`);
     }
-    else {
-        return n * factorial(n - 1);
+
+    return response.json();
+};
+
+async function main(){
+    try {
+        const data = await fetchData();
+        console.log("Dados obtidos:", data)
+    } catch (error) {
+        console.error("Erro:", error);
     }
 }
 
-function add(a,b) {
-    return a + b;
-}
 
-const numbers1 = [1,2,3];
-const dobledNumbers = numbers1.map(num => num * 2);
-
-
-const add = x => x + 1;
-const multiply = x => x * 2;
-const composedFunction = compose(add, multiply);
-console.log(composedFunction(3)); // Primeiro multiplica por 2 e depois adiciona 1
-
-const add1 = x => x + y;
-const add5 = add(5);
-console.log(add5(3)); // Resultado: 8
-
-const numbers2 = [1,2,3,4,5];
-const sum = numbers2.reduce((acc,num) => acc + num, 0)
-
-const newArray = oroginalArray.map(/* element =>  tranformação */);
-
-const numbers3 = [1,2,3,4,5];
-const squaredNumbers = numbers3.map(num => num * num);
-console.log(squaredNumbers);
+main();
+console.log("Aguardando resultado...");
---
+++ Aula2/js/exercicios2.js
@@ -0,0 +1,31 @@
+
+async function fetchData() {
+
+    const cpf = document.getElementById("cpf")
+    const url = `https://viacep.com.br/ws/${cpf}/json/`;
+    
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.status}`);
+    }
+    
+    return response.json();
+};
+
+async function consultaCPF(){
+    try {
+        const data = await fetchData();
+        document.getElementById("dados").innerHTML = data
+    } catch (error) {
+        document.getElementById("dados").innerHTML = "Falha ao buscar o CEP"
+    }
+}
+
+consultaCPF();
+console.log("Aguardando resultado...");
+
+const botaoCep = document.getElementById("button-cep")
+
+botaoCep.addEventListener('click',() => {
+    consultaCPF()
+})
