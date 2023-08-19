607982be49185d02578540deab1ce795cefd941f
Endrio-Korb <alexkorb95@gmail.com>
2023-08-19 17:46:45 -0300

    .

--- Aula3/exercicioApi2.js
+++ Aula3/exercicioApi2.js
@@ -1,80 +1,73 @@
 var prompt = require('prompt-sync')();
 
-
 let name = prompt('Qual o nome do animal? ')
 let species = prompt('Qual a especie do animal?')
 let color = prompt('Qual a cor do animal?')
 let size = promt('Qual o tamanho do animal?')
 
+dados({name:name, species:species, color:color, size:size})
+    
+
+
+// // Listar dados
+// fetch("http://cafepradev.com.br:21020/animals/list")
+//     .then(response = response.json())
+//     .then(reponse => {
+//         console.log(reponse);
+//     })    
+//     .catch(err => console.log(err))
+
 
 // Inserir dados
-const inserirDados = {
-    method : 'POST',
-    headers : {
-        'Content-Type' : 'application/json'
-    },
-}
-dados({name:name, species:species, color:color, size:size})
-
 function inserirDados(dados){
-    fetch("http://cafepradev.com.br:21020/animals/insert", dados)
+    let options = {
+        method : 'POST', 
+        headers : {'Content-Type' : 'application/json' },
+        body : `${dados}`
+    }
+    fetch("http://cafepradev.com.br:21020/animals/insert",options)
     .then(response => response.json())
     .then(response => {
-        response.status
+        return (response.status == 200) ? response.message : false;
     })    
-    .catch(err => console.log(err))
+    .catch(err => {
+        return (err) ? false : true       
+    })
 }    
 
 inserirDados(dados)
 
 
-
-// Listar dados
-fetch("http://cafepradev.com.br:21020/animals/list")
-    .then(response = response.json())
-    .then(dados => {
-        console.log(dados);
-    })    
-    .catch(err => console.log(err))
-
-    
-
-
-// Atualizar dados
-const atualizarDados = {
-    method : 'PUT',
-    headers : {
-        'Content-Type' : 'application/json'
-    },
-    body: `{
-        "name": "Duque",
-        "species": "Dog",
-        "color": "Black",
-        "size": "Small"
-    }`
-}
-fetch("http://cafepradev.com.br:21020/animals/update", atualizarDados)
-    .then(response => response.json())
-    .then(response => {
-        response.status
-    })
-    .catch(err => console.log(err))
+// // Atualizar dados
+// const atualizarDados = {
+//     method : 'PUT',
+//     headers : {
+//         'Content-Type' : 'application/json'
+//     },
+//     body: `${dados}`
+// }
+// fetch("http://cafepradev.com.br:21020/animals/update", atualizarDados)
+//     .then(response => response.json())
+//     .then(response => {
+//         response.status
+//     })
+//     .catch(err => console.log(err))
 
 
 
-// Deletar dados
-const deleteDadosados = {
-    method : 'DELETE',
-    headers: {
-        'Content-Type' : 'application/json'
-    },
-    body :`{
-        "id": 1
-    }`
-}
-fetch("http://cafepradev.com.br:21020/animals/delete", deleteDados)
-    .then(response => response.json())
-    .then(response => {
-        response.status
-    })
-    .catch(err => console.log(err))
+// // Deletar dados
+// const deleteDadosados = {
+//     method : 'DELETE',
+//     headers: {
+//         'Content-Type' : 'application/json'
+//     },
+//     body :`{
+//         "id": 1
+//     }`
+// }
+// fetch("http://cafepradev.com.br:21020/animals/delete", deleteDados)
+//     .then(response => response.json())
+//     .then(response => {
+//         response.status
+//     })
+//     .catch(err => console.log(err))
